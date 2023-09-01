6edb2d8539a2f807a4dc5490b95aa096191b26db
Roman Bartushak <roman.bartushak@viaduct.pro>
2023-09-01 11:52:09 +0300

    add save video modal

--- src/components/about-set/AboutSet.jsx
+++ src/components/about-set/AboutSet.jsx
@@ -5,52 +5,65 @@ import visaSvg from "../../assets/images/visa.svg";
 import mastercardSvg from "../../assets/images/mastercard.svg";
 import globeSvg from "../../assets/images/globe.svg";
 import Accordion from "../accordion";
+import Modal from "../modal/Modal";
 
 const AboutSet = ({ isClosing, selectedSet }) => {
+  const [videoSavedModal, setVideoSavedModal] = React.useState(false);
+
+  const closeModal = () => setVideoSavedModal(false);
+
   return (
-    <div className={`aboutSet ${isClosing && "aboutSetClosing"}`}>
-      <div className="aboutSetHead">
-        <h3 className="aboutSetTitle">Твій кастомний сет:</h3>
-        <h3 className="aboutSetPrice">$50</h3>
-      </div>
+    <>
+      <Modal isVisible={videoSavedModal} closeModal={closeModal} />
+      <div className={`aboutSet ${isClosing && "aboutSetClosing"}`}>
+        <div className="aboutSetHead">
+          <h3 className="aboutSetTitle">Твій кастомний сет:</h3>
+          <h3 className="aboutSetPrice">$50</h3>
+        </div>
 
-      <button className="aboutSetBuyBtn">ДОДАТИ В КОШИК</button>
-      <button className="aboutSetShareBtn">Share to Instagram</button>
+        <button className="aboutSetBuyBtn">ДОДАТИ В КОШИК</button>
+        <button
+          className="aboutSetShareBtn"
+          onClick={() => setVideoSavedModal(true)}
+        >
+          Share to Instagram
+        </button>
 
-      <div className="aboutSetBox">
-        <h4 className="aboutSetYouWillGetTitle">В цьому сеті ти отримаєш:</h4>
-        {selectedSet.youWillGet.map((comp) => (
-          <div className="aboutSetYouWillGetGoods" key={comp.name}>
-            <img
-              className="aboutSetYouWillGetPicture"
-              src={comp.src}
-              alt={comp.name}
-            />
-            <p className="aboutSetYouWillGetName">{comp.name}</p>
+        <div className="aboutSetBox">
+          <h4 className="aboutSetYouWillGetTitle">В цьому сеті ти отримаєш:</h4>
+          {selectedSet.youWillGet.map((comp) => (
+            <div className="aboutSetYouWillGetGoods" key={comp.name}>
+              <img
+                className="aboutSetYouWillGetPicture"
+                src={comp.src}
+                alt={comp.name}
+              />
+              <p className="aboutSetYouWillGetName">{comp.name}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="aboutSetBox">
+          <div className="aboutSetPayWith">
+            <img className="aboutSetPayWithMainIcon" src={cardSvg} alt="card" />
+            <span>Pay with credit card</span>
+            <img src={visaSvg} alt="visa" />
+            <img src={mastercardSvg} alt="mastercard" />
           </div>
-        ))}
-      </div>
-
-      <div className="aboutSetBox">
-        <div className="aboutSetPayWith">
-          <img className="aboutSetPayWithMainIcon" src={cardSvg} alt="card" />
-          <span>Pay with credit card</span>
-          <img src={visaSvg} alt="visa" />
-          <img src={mastercardSvg} alt="mastercard" />
+          <div className="aboutSetPayWith">
+            <img
+              className="aboutSetPayWithMainIcon"
+              style={{ padding: "0 4px" }}
+              src={globeSvg}
+              alt="globe"
+            />
+            <span>Worldwide shipping</span>
+          </div>
         </div>
-        <div className="aboutSetPayWith">
-          <img
-            className="aboutSetPayWithMainIcon"
-            style={{ padding: "0 4px" }}
-            src={globeSvg}
-            alt="globe"
-          />
-          <span>Worldwide shipping</span>
-        </div>
-      </div>
 
-      <Accordion aboutProduct={selectedSet.aboutProduct} />
-    </div>
+        <Accordion aboutProduct={selectedSet.aboutProduct} />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./modal.css";
+
+const Modal = ({ isVisible, closeModal }) => {
+  const [isClosing, setIsClosing] = React.useState(false);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  const closeModalWithAnimation = () => {
+    setIsClosing(true);
+
+    setTimeout(() => {
+      closeModal();
+      setIsClosing(false);
+    }, 500);
+  };
+
+  return (
+    <div
+      className={`candlesModalOverlay ${
+        isClosing && "candlesModalOverlayClosing"
+      }`}
+    >
+      <div className="candlesModal">
+        <div className="candlesModalSuccessIcon">
+          <svg
+            width="60"
+            height="40"
+            viewBox="0 0 60 40"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3 17.5L23 37.5L57.5 3"
+              stroke="black"
+              stroke-width="5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+        <h3 className="candlesModalTitle">
+          Відео з вашим сетом збережено в галерею
+        </h3>
+        <button className="aboutSetBuyBtn" onClick={closeModalWithAnimation}>
+          OK
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
