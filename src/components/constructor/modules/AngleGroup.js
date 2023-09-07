2d21d94535bf67f649b9a4f9c84cfac59414d914
Roman Bartushak <roman.bartushak@viaduct.pro>
2023-09-07 15:50:33 +0300

    return align angle as it was

--- src/components/constructor/modules/AngleGroup.js
+++ src/components/constructor/modules/AngleGroup.js
@@ -2,16 +2,16 @@ import { useFrame } from "@react-three/fiber";
 import React from "react";
 
 const AngleGroup = ({ scale, children, isAngle }) => {
-  const [angle, setAngle] = React.useState(0);
+  const [angle, setAngle] = React.useState(0.3);
 
-  // useFrame(() => {
-  //   if (angle < 0.3 && isAngle) {
-  //     setAngle((a) => a + 0.01);
-  //   }
-  //   if (angle > 0 && !isAngle) {
-  //     setAngle((a) => a - 0.01);
-  //   }
-  // });
+  useFrame(() => {
+    if (angle < 0.3 && isAngle) {
+      setAngle((a) => a + 0.01);
+    }
+    if (angle > 0 && !isAngle) {
+      setAngle((a) => a - 0.01);
+    }
+  });
 
   return (
     <group scale={scale} rotation={[angle, 0, 0]}>
