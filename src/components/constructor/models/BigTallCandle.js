4cd8dcf133e457113db3ec58b52e73a203754f76
Roman Bartushak <roman.bartushak@viaduct.pro>
2023-09-07 19:34:53 +0300

    fix models nameing issue

--- src/components/constructor/models/BigTallCandle.js
+++ src/components/constructor/models/BigTallCandle.js
@@ -1,7 +1,7 @@
 import Model from "./Model";
 
 const BigTallCandle = (props) => (
-  <Model {...props} raiseOn={0.3} model="BigTall.gltf" />
+  <Model {...props} raiseOn={0.3} model="BigTallModel.gltf" />
 );
 
 export default BigTallCandle;
--- src/components/constructor/models/ThinShortCandle.js
+++ src/components/constructor/models/ThinShortCandle.js
@@ -1,7 +1,7 @@
 import Model from "./Model";
 
 const ThinShortCandle = (props) => (
-  <Model {...props} raiseOn={0.3} model="ThinShort.gltf" yPosition={0.628} />
+  <Model {...props} raiseOn={0.3} model="ThinShortModel.gltf" yPosition={0.628} />
 );
 
 export default ThinShortCandle;
