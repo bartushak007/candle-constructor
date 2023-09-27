bf2f5f04ef398ca2dbff806a61967141b21926d5
Roman Bartushak <roman.bartushak@viaduct.pro>
2023-09-27 13:26:26 +0300

    throttle rotation

--- src/components/constructor/useCustomRotate.js
+++ src/components/constructor/useCustomRotate.js
@@ -1,3 +1,4 @@
+import throttle from "lodash.throttle";
 import React from "react";
 
 const useCustomRotate = (orbitControlsRef) => {
@@ -7,6 +8,7 @@ const useCustomRotate = (orbitControlsRef) => {
     y: 0.3,
   });
 
+  const prevEventClientPositions = React.useRef(null);
   const prevRotatePositions = React.useRef(null);
 
   const handleMouseDown = (event) => {
@@ -15,30 +17,55 @@ const useCustomRotate = (orbitControlsRef) => {
 
   const handleMouseUp = () => {
     setIsRotating(false);
-    prevRotatePositions.current = null;
+    prevEventClientPositions.current = null;
   };
+
+  const throttleRotatePosition = React.useMemo(() => {
+    return throttle(
+      (x, y) =>
+        setRotatePositions((p) => {
+          const prev = prevRotatePositions.current || p;
+          return {
+            ...prev,
+          };
+        }),
+      20
+    );
+  }, []);
 
   const handleMouseMove = (event) => {
     if (isRotating) {
       const { clientX, clientY } = event;
 
-      if (prevRotatePositions.current) {
+      if (prevEventClientPositions.current) {
         const currentZoom =
           orbitControlsRef.current?.object.position.z / 1000 || 0.006;
-        const zoom = currentZoom > 0.006 ? 0.006 : currentZoom < 0.001 ? 0.001 : currentZoom;
+        const zoom =
+          currentZoom > 0.006
+            ? 0.006
+            : currentZoom < 0.001
+            ? 0.001
+            : currentZoom;
 
-        const x = (clientX - prevRotatePositions.current.clientX) * zoom;
-        const y = (clientY - prevRotatePositions.current.clientY) * zoom;
+        const x = (clientX - prevEventClientPositions.current.clientX) * zoom;
+        const y = (clientY - prevEventClientPositions.current.clientY) * zoom;
 
-        setRotatePositions((prev) => {
-          return {
-            x: prev.x + x,
-            y: prev.y + y > 1.6 ? 1.6 : prev.y + y <= -1.6 ? -1.6 : prev.y + y,
-          };
-        });
+        const prev = prevRotatePositions.current || rotatePositions;
+        prevRotatePositions.current = {
+          x: prev.x + x,
+          y: prev.y + y > 1.6 ? 1.6 : prev.y + y <= -1.6 ? -1.6 : prev.y + y,
+        };
+
+        throttleRotatePosition(x, y);
+        // setRotatePositions((prev) => {
+        //   return {
+        //     x: prev.x + x,
+        //     y: prev.y + y > 1.6 ? 1.6 : prev.y + y <= -1.6 ? -1.6 : prev.y + y,
+        //   };
+        // });
       }
 
-      prevRotatePositions.current = { clientX, clientY };
+      prevEventClientPositions.current = { clientX, clientY };
     }
   };
 
@@ -48,11 +75,11 @@ const useCustomRotate = (orbitControlsRef) => {
     if (event.touches.length === 1) {
       const { clientX, clientY } = event.touches[0];
 
-      if (prevRotatePositions.current) {
+      if (prevEventClientPositions.current) {
         const zoom = orbitControlsRef.current?.object.position.z / 1000 || 0.01;
 
-        const x = (clientX - prevRotatePositions.current.clientX) * zoom;
-        const y = (clientY - prevRotatePositions.current.clientY) * zoom;
+        const x = (clientX - prevEventClientPositions.current.clientX) * zoom;
+        const y = (clientY - prevEventClientPositions.current.clientY) * zoom;
 
         setRotatePositions((prev) => {
           return {
@@ -62,12 +89,12 @@ const useCustomRotate = (orbitControlsRef) => {
         });
       }
 
-      prevRotatePositions.current = { clientX, clientY };
+      prevEventClientPositions.current = { clientX, clientY };
     }
   };
 
   const handleTouchEnd = () => {
-    prevRotatePositions.current = null;
+    prevEventClientPositions.current = null;
   };
 
   return {
